28c8916144521b3b401a79077740d137f8821670
Keith Hilde <constspcltsfargo@gmail.com>
2025-10-31 17:10:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6165246273-4d6aa",
+  "appId": "1:942250916033:web:39f8d3e7350125271c7c40",
+  "apiKey": "AIzaSyC8vBZ7AjzR1EzhIDqQ040798WyrFwjNuI",
+  "authDomain": "studio-6165246273-4d6aa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "942250916033"
+};
