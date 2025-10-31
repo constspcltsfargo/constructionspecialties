807213043f56081ec3b247bebd779268d5a47f70
Keith Hilde <constspcltsfargo@gmail.com>
2025-10-31 17:56:03 +0000

    give me the format for env file
    // Import the functions you need from t

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6165246273-4d6aa",
-  "appId": "1:942250916033:web:39f8d3e7350125271c7c40",
-  "apiKey": "AIzaSyC8vBZ7AjzR1EzhIDqQ040798WyrFwjNuI",
-  "authDomain": "studio-6165246273-4d6aa.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "942250916033"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
