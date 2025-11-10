5f383054511b29666ed4fdc631a5c6704dc67634
Keith Hilde <constspcltsfargo@gmail.com>
2025-11-10 07:52:45 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,15 @@
+
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  // const token = request.cookies.get('firebaseIdToken');
+  const token = request.cookies.get('firebaseIdToken');
 
-  // if (request.nextUrl.pathname.startsWith('/admin')) {
-  //   if (!token) {
-  //     return NextResponse.redirect(new URL('/login', request.url));
-  //   }
-  // }
+  if (request.nextUrl.pathname.startsWith('/admin')) {
+    if (!token) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
 
   return NextResponse.next();
 }
